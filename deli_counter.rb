1a11cc8cf03eade3115d7d1d7254fe446cb6b14c
York Wang <yw544@nyu.edu>
2018-01-04 20:02:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,8 +8,8 @@ def line(katz_deli)
 
       string = "The line is currently: "
       katz_deli.each_with_index do |name, index|
-      string = "#{string} #{index+1}. #{name}"
+      string_new = "#{string} #{index+1}. #{name}"
     end
-    puts string
+    puts string_new
   end
 end
