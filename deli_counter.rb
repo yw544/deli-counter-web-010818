d166cffe28e10dc825ea11737837ab44bb3b109e
York Wang <yw544@nyu.edu>
2018-01-04 20:12:51 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+
+  else
+      string = "The line is currently:"
+      katz_deli.each_with_index do |name,index|
+
+        string += " #{index+1}. #{name}"
+    end
+    puts string
+  end
+end
+
+
+def take_a_number(katz_deli,name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length+1} in line."
+  katz_deli.push(name)
+end
+
+
+def now_serving(katz_deli)
+if katz_deli.length == 0
+  puts "There is nobody waiting to be served!"
+else
+  puts "Currently serving #{katz_deli.shift}."
+end
+end  
