ae0f2a6c2f0250b500f5fd880048cf2b15e9e756
York Wang <yw544@nyu.edu>
2018-01-04 20:12:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,17 @@ def line(katz_deli)
   end
 end
 
+
 def take_a_number(katz_deli,name)
   puts "Welcome, #{name}. You are number #{katz_deli.length+1} in line."
   katz_deli.push(name)
 end
+
+
+def now_serving(katz_deli)
+if katz_deli.length == 0
+  puts "There is nobody waiting to be served!"
+else
+  puts "Currently serving #{katz_deli.shift}."
+end
+end  
