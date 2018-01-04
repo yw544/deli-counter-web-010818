339fe244f896932cf4a660e8b2be5107744e81e4
York Wang <yw544@nyu.edu>
2018-01-04 19:54:15 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |name, index|
+      string = "the line is currently: #{index+1}. #{name}"
+    end 
+    puts string
+  end 
+end 
